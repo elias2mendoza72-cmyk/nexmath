49b1758b9307e6f82d279aec61a9b9a13fb09093
Elias Mendoza <elias2mendoza72@gmail.com>
2026-02-05 16:04:46 -0800

    Initial Commit

---
+++ NexMathiOSTests/NexMathiOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  NexMathiOSTests.swift
+//  NexMathiOSTests
+//
+//  Created by Elias Mendoza on 2/5/26.
+//
+
+import Testing
+@testable import NexMathiOS
+
+struct NexMathiOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
